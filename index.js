913aedafa9db6536cb1c326f2ae1658887005f44
AliAbbasGharib <ali.gharib560@gmail.com>
2025-05-11 21:46:18 +0300

    Use env for MongoDB URI

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://seniorproject-3.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 }));
 
