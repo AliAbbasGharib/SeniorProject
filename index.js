297920a4689d064a661a9b1587723788029a306c
AliAbbasGharib <ali.gharib560@gmail.com>
2025-05-26 22:06:44 +0300

    update url in cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(cors({
     origin: ['https://seniorproject-1-3rbo.onrender.com', 'http://localhost:3000',
-        'https://683366b58d3b000008afb666--redlink12.netlify.app'],
+        'https://6834bb558c860300080e0057--redlink12.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
