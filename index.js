3719b372112487e38bc44a2a6f092cc68fda62b9
AliAbbasGharib <ali.gharib560@gmail.com>
2025-05-28 22:10:01 +0300

    edit url in cdn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(cors({
     origin: ['https://seniorproject-1-3rbo.onrender.com', 'http://localhost:3000',
-        'https://6834bb558c860300080e0057--redlink12.netlify.app'],
+        'https://68375f0a4e7bb80008a2e166--redlink12.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
