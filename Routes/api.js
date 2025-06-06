2bb253ce4ca1f987e40e45f6985c0ec47657b153
AliAbbasGharib <ali.gharib560@gmail.com>
2025-06-06 10:38:00 +0300

    update

--- Routes/api.js
+++ Routes/api.js
@@ -100,5 +100,4 @@ router.post('/answer', async (req, res) => {
 });
 
 
-
 module.exports = router;
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("connected succufully");
