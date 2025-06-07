021c30a160b58cc55b861f4579fee321ecd2a155
AliAbbasGharib <ali.gharib560@gmail.com>
2025-06-07 16:37:42 +0300

    Add Userpagination Controlle

--- app/Controller/AuthController.js
+++ app/Controller/AuthController.js
@@ -1,39 +1,18 @@
 const jwt = require('jsonwebtoken');
-const twilio = require('twilio');
 const bcrypt = require('bcryptjs');
 const User = require("../../Models/Users");
 
 const createToken = (id) => {
-    return jwt.sign({ id },  process.env.JWT_SECRET, {
+    return jwt.sign({ id }, "process.env.JWT_SECRET", {
         expiresIn: '180d'
     });
 };
-
-function generateCode() {
-    return Math.floor(100000 + Math.random() * 900000).toString();
-}
-
-// Send SMS (replace with actual RealAuth API details)
-async function sendWhatsApp(phone, code) {
-    try {
-        const fullPhone = phone.startsWith('whatsapp:') ? phone : `whatsapp:${phone}`;
-        const message = await client.messages.create({
-            from: process.env.TWILIO_WHATSAPP_NUMBER,
-            to: fullPhone,
-            body: `Your verification code is: ${code}`
-        });
-        return message.sid;
-    } catch (error) {
-        console.error("WhatsApp Error:", error.message);
-        throw new Error("Failed to send WhatsApp message");
-    }
-}
 
 // register 
 module.exports.register = async (req, res) => {
     try {
         const { name, email, password, phone_number, date_of_birth, gender, blood_type,
-            address, last_donation_date, location,
+            address, last_donation_date,location,
             role } = req.body;
 
         const existingUser = await User.findOne({ email });
@@ -60,8 +39,6 @@ module.exports.register = async (req, res) => {
         await newUser.save();
 
         const token = createToken(newUser._id);
-        const code = generateCode();
-        await sendWhatsApp(phone_number, code);
 
         res.status(201).json({
             user: newUser,
