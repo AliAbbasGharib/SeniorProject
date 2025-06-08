3d5adcd5ea116c354fa90c8fa20a6589c945eb15
AliAbbasGharib <ali.gharib560@gmail.com>
2025-06-08 13:00:27 +0300

    Add Verify Email

--- Routes/api.js
+++ Routes/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const AuthController = require('../app/Controller/AuthController');
-const 
 const UserController = require("../app/Controller/UserController");
 const AuthMiddleware = require('../app/Middleware/Authenticate');
 const RateLimiter = require('../app/Middleware/RateLimiter');
