8bb5117068b1ef70081ba5538d16469e96f59ddb
AliAbbasGharib <ali.gharib560@gmail.com>
2025-06-13 06:47:18 +0300

    update

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,10 +1,10 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../seniorproject-be8ce-firebase-adminsdk-fbsvc-0580e1044f.json'); // adjust path as needed
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS); // adjust path as needed
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  
+
 });
-console.log("Firebase client_email:", admin);
+console.log("Firebase client_email:", serviceAccount.client_email); // Log the client_email for debugging
 
 module.exports = admin;
